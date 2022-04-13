55d3d2cccad294d4c16ffd93f7ced76857ea75a1
K.R <kewin.allenspach@gmail.com>
2022-04-13 11:59:23 +0200

    Adapting to Monobehaviour

--- Assets/Scripts/DamageCalculator.cs
+++ Assets/Scripts/DamageCalculator.cs
@@ -10,7 +10,7 @@ public class DamageCalculator : MonoBehaviour
     }
 
     public static int CalculateDamage(int amount, Character character){
-        int totalArmor = character.Inventory.GetTotalArmor();
+        int totalArmor = character.Inventory.GetTotalArmor() + (character.Level * 10);
         float multiplier = 100f - totalArmor;
         multiplier /= 100f;
         return Convert.ToInt32(amount*multiplier);
